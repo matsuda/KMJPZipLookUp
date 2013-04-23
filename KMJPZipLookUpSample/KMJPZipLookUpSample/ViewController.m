43d53a09d46cb2b784ac5f403ed6023f86eada7a
matsuda <hi@matsuda.me>
2013-04-23 15:26:43 +0900

    ライブラリを使う側のコードを実装（iPhone）

--- KMJPZipLookUpSample/KMJPZipLookUpSample/ViewController.m
+++ KMJPZipLookUpSample/KMJPZipLookUpSample/ViewController.m
@@ -44,6 +44,7 @@
 
 - (IBAction)lookUp:(id)sender
 {
+    [self.view endEditing:YES];
     [self requestAPIWithZipcode:self.textField.text];
 }
 
@@ -57,22 +58,26 @@
         return;
     }
 
+    [self showIndicator];
+
     __weak typeof(self) wself = self;
     [[KMJPZipLookUpClient sharedClient] lookUpWithZipcode:zipcode success:^(AFHTTPRequestOperation *operation, KMJPZipLookUpResponse *response){
-        NSLog(@"success >>>>>>>> %@", response);
+//        NSLog(@"success >>>>>>>> %@", response);
         _response = response;
         [wself didFinishCheckZipSearchAPI];
     } failure:^(AFHTTPRequestOperation *operation, NSError *error){
-        NSLog(@"failure >>>>>>>> %@", error);
+//        NSLog(@"failure >>>>>>>> %@", error);
         [wself didFailCheckZipSearchAPIWithError:error];
     }];
 }
 
 - (void)didFinishCheckZipSearchAPI
 {
+    [self hideIndicator];
+
     NSArray *addresses = _response.addresses;
     if ([addresses count] > 0) {
-        [self presentAddressListPopover];
+        [self presentAddressListViewController];
     } else {
         [self showAlertNotFoundAddressListWithMessage:@"該当する住所が見つかりませんでした。"];
     }
@@ -80,6 +85,8 @@
 
 - (void)didFailCheckZipSearchAPIWithError:(NSError *)error
 {
+    [self hideIndicator];
+
     NSString *message = [error localizedDescription];
     [self showAlertNotFoundAddressListWithMessage:message];
 }
@@ -92,11 +99,7 @@
     KMJPZipLookUpAddress *address = [userInfo objectForKey:kKMJPZipLookUpAddressListResultsKey];
     self.label.text = [address fullAddress];
 
-    [[NSNotificationCenter defaultCenter] removeObserver:self name:kKMJPZipLookUpAddressListDidSelectNotification object:nil];
-
-    UIPopoverController *popover = [self addressListPopover];
-    [popover dismissPopoverAnimated:YES];
-    _popover = nil;
+    [self dismissAddressListViewController];
 }
 
 - (void)showAlertNotFoundAddressListWithMessage:(NSString *)message
@@ -105,12 +108,46 @@
     [alert show];
 }
 
-- (void)presentAddressListPopover
+- (KMJPZipLookUpAddressListViewController *)addressListViewController
+{
+    KMJPZipLookUpAddressListViewController *controller = [[KMJPZipLookUpAddressListViewController alloc] initWithStyle:UITableViewStylePlain];
+    controller.response = _response;
+    return controller;
+}
+
+- (void)presentAddressListViewController
 {
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(didSelectAddress:) name:kKMJPZipLookUpAddressListDidSelectNotification object:nil];
 
+    if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) {
+        [self presentAddressListPopover];
+    } else {
+        [self presentAddressListModalController];
+    }
+}
+
+- (void)dismissAddressListViewController
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:kKMJPZipLookUpAddressListDidSelectNotification object:nil];
+
+    if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) {
+        UIPopoverController *popover = [self addressListPopover];
+        [popover dismissPopoverAnimated:YES];
+        _popover = nil;
+    } else {
+        [self.presentedViewController dismissViewControllerAnimated:YES completion:nil];
+    }
+}
+
+- (void)presentAddressListModalController
+{
+    KMJPZipLookUpAddressListViewController *controller = [self addressListViewController];
+    [self presentViewController:controller animated:YES completion:nil];
+}
+
+- (void)presentAddressListPopover
+{
     UIPopoverController *popover = [self addressListPopover];
-    [(KMJPZipLookUpAddressListViewController *)popover.contentViewController setResponse:_response];
     [popover setPopoverContentSize:CGSizeMake(400.f, 132.f)];
     [popover presentPopoverFromRect:self.textField.frame inView:self.view permittedArrowDirections:UIPopoverArrowDirectionLeft animated:YES];
 }
@@ -121,7 +158,8 @@
         return _popover;
     }
 
-    KMJPZipLookUpAddressListViewController *popoverContent = [[KMJPZipLookUpAddressListViewController alloc] initWithStyle:UITableViewStylePlain];
+    KMJPZipLookUpAddressListViewController *popoverContent = [self addressListViewController];
+    popoverContent.modalInPopover = YES;
     _popover = [[UIPopoverController alloc] initWithContentViewController:popoverContent];
     _popover.delegate = self;
     return _popover;
@@ -133,6 +171,43 @@
 {
     if ([popoverController isEqual:_popover]) {
         _popover = nil;
+    }
+}
+
+- (void)showIndicator
+{
+    UIView *grayer = [[UIView alloc] initWithFrame:self.view.bounds];
+    grayer.backgroundColor = [UIColor blackColor];
+    grayer.alpha = 0.5;
+    grayer.autoresizesSubviews = YES;
+    grayer.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth;
+    [self.view addSubview:grayer];
+
+    UIActivityIndicatorView *indicator = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhiteLarge];
+    indicator.autoresizingMask = (UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleBottomMargin |
+                                  UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleRightMargin );
+    indicator.center = CGPointMake(self.view.bounds.size.width / 2, self.view.bounds.size.height / 2);
+    [indicator startAnimating];
+    [self.view addSubview:indicator];
+}
+
+- (void)hideIndicator
+{
+    if ([self.view.subviews count] >= 2) {
+        NSEnumerator *enumerator = [self.view.subviews reverseObjectEnumerator];
+        BOOL isExist = NO;
+        for (UIView *v in enumerator) {
+            if (isExist) {
+                [v removeFromSuperview];
+                break;
+            }
+            if ([v isKindOfClass:[UIActivityIndicatorView class]]) {
+                UIActivityIndicatorView *indicator = (UIActivityIndicatorView *)v;
+                [indicator stopAnimating];
+                [indicator removeFromSuperview];
+                isExist = YES;
+            }
+        }
     }
 }
 
